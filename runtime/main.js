137a4a82b1d7c54bad0d91a8daccb06a84bf23a6
defor1 <127444171+defor1@users.noreply.github.com>
2025-05-16 13:30:05 +0300

    Add files via upload

---
+++ runtime/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const uiPath = path.join(app.getPath("userData"), "ui", "index.html");
+  const mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+  mainWindow.loadFile(uiPath);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
